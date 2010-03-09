d537df9fe95837911da37115dfc4095d35059cbe
laaglu <laaglu@d5b21e57-afc5-4760-a3f5-3efab009ca8d>
2010-03-09 13:45:58 +0000

    Tagging the 0.3 version

---
+++ tags/0.3/src/main/java/org/vectomatic/svg/samples/client/Main.java
@@ -0,0 +1,102 @@
+/**********************************************
+ * Copyright (C) 2009 Lukas Laag
+ * This file is part of libgwtsvg-samples.
+ * 
+ * libgwtsvg-samples is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libgwtsvg-samples is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with libgwtsvg-samples.  If not, see http://www.gnu.org/licenses/
+ **********************************************/
+package org.vectomatic.svg.samples.client;
+
+import org.vectomatic.svg.samples.client.events.EventSample;
+import org.vectomatic.svg.samples.client.parser.ParserSample;
+import org.vectomatic.svg.samples.client.shapes.ShapesSample;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.DecoratorPanel;
+import com.google.gwt.user.client.ui.HorizontalSplitPanel;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeItem;
+
+public class Main implements EntryPoint {
+	interface MainBinder extends UiBinder<DecoratorPanel, Main> {
+	}
+	private static MainBinder binder = GWT.create(MainBinder.class);
+
+	/**
+	 * The type passed into the
+	 * {@link org.vectomatic.svg.samples.generator.SourceGenerator}.
+	 */
+	private static final class GeneratorInfo {
+	}
+	
+	@UiField
+	HorizontalSplitPanel splitPanel;
+	@UiField
+	Tree tree;
+	@UiField
+	SimplePanel sampleContainer;
+
+	public int getHeight() {
+		return (Window.getClientHeight() - 150);
+	}
+
+	@Override
+	public void onModuleLoad() {
+	    // Generate the source code for the examples
+	    GWT.create(GeneratorInfo.class);
+	    DecoratorPanel panel = binder.createAndBindUi(this);
+	    
+		// Handle resizing issues.
+		ResizeHandler resizeHandler = new ResizeHandler() {
+			@Override
+			public void onResize(ResizeEvent event) {
+				String width = (Window.getClientWidth() - 20) + "px";
+				String height = getHeight() + "px";
+				splitPanel.setSize(width, height);
+			}
+		};
+		Window.addResizeHandler(resizeHandler);
+		resizeHandler.onResize(null);
+		splitPanel.setSplitPosition("20%");
+
+		// Populate the sample tree
+	    TreeItem shapesSample = tree.addItem("shapes");
+	    shapesSample.setUserObject(new ShapesSample());
+	    TreeItem eventSample = tree.addItem("events");
+	    eventSample.setUserObject(new EventSample());
+	    TreeItem parserSample = tree.addItem("parser");
+	    parserSample.setUserObject(new ParserSample());
+	    tree.addSelectionHandler(new SelectionHandler<TreeItem>() {
+			@Override
+			public void onSelection(SelectionEvent<TreeItem> event) {
+				SampleBase sample = (SampleBase) event.getSelectedItem().getUserObject();
+				Panel samplePanel = sample.getPanel();
+				sampleContainer.setWidget(samplePanel);
+			}
+	    	
+	    });
+	    tree.setSelectedItem(shapesSample);
+	    RootPanel.get("uiRoot").add(panel);
+	}
+}
---
+++ tags/0.3/src/main/java/org/vectomatic/svg/samples/client/parser/ParserSample.java
@@ -0,0 +1,108 @@
+/**********************************************
+ * Copyright (C) 2009 Lukas Laag
+ * This file is part of libgwtsvg-samples.
+ * 
+ * libgwtsvg-samples is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libgwtsvg-samples is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with libgwtsvg-samples.  If not, see http://www.gnu.org/licenses/
+ **********************************************/
+package org.vectomatic.svg.samples.client.parser;
+
+import org.vectomatic.dom.svg.OMSVGSVGElement;
+import org.vectomatic.dom.svg.utils.OMSVGParser;
+import org.vectomatic.svg.samples.client.SampleBase;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SimplePanel;
+
+/**
+ * Class to demonstrate the parsing of SVG files
+ * @author laaglu
+ */
+public class ParserSample extends SampleBase {
+	interface ParserSampleBinder extends UiBinder<SimplePanel, ParserSample> {
+	}
+	private static ParserSampleBinder binder = GWT.create(ParserSampleBinder.class);
+
+	@UiField
+	HTML svgContainer;
+	@UiField
+	ListBox documentListBox;
+	
+	@Override
+	public Panel getPanel() {
+		if (panel == null) {
+			// Initialize the UI with UiBinder
+			panel = binder.createAndBindUi(this);
+			requestSourceContents(HTML_SRC_DIR + "ParserSample" + ".html");
+			tabPanel.getTabBar().setTabText(0, "Parser");
+			tabPanel.getTabBar().setTabText(1, "HTML");
+			tabPanel.selectTab(0);
+
+			// Fill the list box with svg file names
+			documentListBox.addItem("tiger", "tiger.svg");
+			documentListBox.addItem("lion", "lion.svg");
+			documentListBox.addItem("butterfly", "butterfly.svg");
+			documentListBox.setSelectedIndex(0);
+			documentListBoxChange(null);
+		}
+		return panel;
+	}
+	
+	@UiHandler("documentListBox")
+	public void documentListBoxChange(ChangeEvent event) {
+		// Request the contents of the file
+		String svgDocument = documentListBox.getValue(documentListBox.getSelectedIndex());
+		String resource = GWT.getModuleBaseURL() + svgDocument;
+		RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, resource);
+		builder.setCallback(new RequestCallback() {
+			public void onError(Request request, Throwable exception) {
+				source.setHTML("Cannot find resource");
+			}
+
+			public void onResponseReceived(Request request, Response response) {
+				// Parse the document
+				OMSVGSVGElement svg = OMSVGParser.parse(response.getText());
+				
+				// Insert the SVG root element into the HTML UI
+				Element div = svgContainer.getElement();
+				div.getStyle().setHeight(600, Style.Unit.PX);
+				if (div.hasChildNodes()) {
+					div.replaceChild(svg.getElement(), div.getFirstChild());
+				} else {
+					div.appendChild(svg.getElement());					
+				}
+			}
+		});
+
+		// Send the request
+		try {
+			builder.send();
+		} catch (RequestException e) {
+			GWT.log("Cannot fetch resource " + resource, e);
+		}
+	}
+}
