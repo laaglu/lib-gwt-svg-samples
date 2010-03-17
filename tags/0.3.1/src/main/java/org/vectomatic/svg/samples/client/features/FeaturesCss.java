9616677770ec4ce019de0b13c79a28b5a9f63fda
laaglu <laaglu@d5b21e57-afc5-4760-a3f5-3efab009ca8d>
2010-03-17 07:40:47 +0000

    Tagging of the 0.3.1 version

---
+++ tags/0.3.1/src/main/java/org/vectomatic/svg/samples/client/features/FeaturesCss.java
@@ -0,0 +1,9 @@
+package org.vectomatic.svg.samples.client.features;
+
+import com.google.gwt.resources.client.CssResource;
+
+public interface FeaturesCss extends CssResource {
+	public String supported();
+	public String unsupported();
+	public String header();
+}
